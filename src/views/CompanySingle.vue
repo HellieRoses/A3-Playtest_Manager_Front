a7538fd9a72adf65d214c22a6b0552e89562f40c
Maëlys Boissezon <maelys.boissezon@etu.umontpellier.fr>
2024-12-10 23:25:10 +0100

    front des companysingle

--- src/views/CompanySingle.vue
+++ src/views/CompanySingle.vue
@@ -32,7 +32,7 @@
         <router-link :to="{name:'videogame'}" class="block"><p>Assassin's Creed</p></router-link>
         <router-link :to="{name:'videogame'}" class="block"><p>Assassin's Creed</p></router-link>
       </div>
-     <div><div class="button"><p>Voir plus</p></div></div>
+     <div><div class="button" @click="$router.push($router.push({name : 'videogames'}))"><p>Voir plus</p></div></div> <!-- TODO mettre la liste des jeux vidéos de l'entreprise -->
     </div>
   </div>
 </template>
