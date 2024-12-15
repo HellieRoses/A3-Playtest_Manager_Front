7e234b177396bcb42db77609e7f6281d87440b42
Maëlys Boissezon <maelys.boissezon@etu.umontpellier.fr>
2024-12-15 22:09:53 +0100

    form inscription player et company

--- src/components/user/RegisterForm.vue
+++ src/components/user/RegisterForm.vue
@@ -1,23 +1,93 @@
 <script setup lang="ts">
+let accountType = 'player';
+let playerForm;
+let companyForm;
+window.onload = () => {
+  playerForm = document.getElementById('playerForm');
+  companyForm = document.getElementById('companyForm');
+  companyForm.style.display = 'none';
+}
 
+function clickOnRound(type){
+  if(accountType != type){
+    accountType=type;
+    let currentYellow = document.getElementById('choice').querySelector('.yellowRound');
+    let other = document.getElementById('choice').querySelector('.round:not(.yellowRound)');
+    currentYellow.classList.remove('yellowRound');
+    other.classList.add('yellowRound');
+    if(type == 'player'){
+      playerForm.style.display= 'flex';
+      companyForm.style.display = 'none';
+    }else if(type == 'company'){
+      playerForm.style.display= 'none';
+      companyForm.style.display = 'flex';
+    }
+  }
+}
 </script>
 
 <template>
   <div class="form">
     <div>
-      <h1>Connexion</h1>
-      <div class="round">
-        <img src="@/assets/img/profile_bigger.png">
+      <h1>Inscription</h1>
+      <div id="choice">
+        <div class="round yellowRound" @click="clickOnRound('player')">
+          <img src="@/assets/img/videoGame.png">
+        </div>
+        <div class="round">
+          <img src="@/assets/img/building_bigger.png" @click="clickOnRound('company')">
+        </div>
       </div>
     </div>
-    <form @submit.prevent=""> <!-- fonction connect -->
+
+    <form @submit.prevent="" id="playerForm"> <!-- fonction inscrire player-->
       <div>
         <div class="group">
-          <input type="text" required/>
+          <input type="text" required placeholder="Votre nom d'utilisateur..."/>
+          <label for="username">Nom d'Utilisateur</label>
+        </div>
+        <div id="names">
+          <div class="group">
+            <input type="text" required placeholder="Votre nom..."/>
+            <label for="username">Nom</label>
+          </div>
+          <div class="group">
+            <input type="text" required placeholder="Votre prénom..."/>
+            <label for="firstname">Prénom</label>
+          </div>
+        </div>
+        <div class="group">
+          <input type="email" required placeholder="Votre email..."/>
+          <label for="email">Email</label>
+        </div>
+        <div class="group">
+          <input type="password" required placeholder="Votre mot de passe..."/>
+          <label for="password">Mot de passe</label>
+        </div>
+      </div>
+      <div class="bottom-button">
+        <button class="button">
+          <p>Se Connecter</p>
+        </button>
+      </div>
+    </form>
+
+    <form @submit.prevent="" id="companyForm"> <!-- fonction inscrire company -->
+      <div>
+        <div class="group">
+          <input type="text" required placeholder="Votre nom d'utilisateur..."/>
           <label for="username">Nom d'Utilisateur</label>
         </div>
         <div class="group">
-          <input type="password" required/>
+          <input type="text" required placeholder="Nom d'entreprise..."/>
+          <label for="companyName">Nom d'Entreprise</label>
+        </div>
+        <div class="group">
+          <input type="email" required placeholder="Votre email..."/>
+          <label for="email">Email</label>
+        </div>
+        <div class="group">
+          <input type="password" required placeholder="Votre mot de passe..."/>
           <label for="password">Mot de passe</label>
         </div>
       </div>
@@ -31,5 +101,25 @@
 </template>
 
 <style scoped>
+@import "@/assets/formsView.css";
+form{
+  padding-top: 5%!important;
+}
+.group{
+  margin: 8px 0;
+}
+#names{
+  width: 60%;
+  display: flex;
+  justify-content: space-between;
 
+  & .group{
+    width: 45%;
+  }
+}
+#choice{
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+}
 </style>
