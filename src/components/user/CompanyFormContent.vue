602107fb2811cf012cdf8b85e2433993b21f5ef7
Maëlys Boissezon <maelys.boissezon@etu.umontpellier.fr>
2024-12-17 22:59:27 +0100

    oubli des name + des type=submit

--- src/components/user/CompanyFormContent.vue
+++ src/components/user/CompanyFormContent.vue
@@ -4,19 +4,19 @@
 
 <template>
   <div class="group">
-    <input id="username" type="text" required placeholder="Votre nom d'utilisateur..."/>
+    <input id="username" name="username" type="text" required placeholder="Votre nom d'utilisateur..."/>
     <label for="username">Nom d'Utilisateur</label>
   </div>
   <div class="group">
-    <input id="companyName" type="text" required placeholder="Nom d'entreprise..."/>
+    <input id="companyName" name="companyName" type="text" required placeholder="Nom d'entreprise..."/>
     <label for="companyName">Nom d'Entreprise</label>
   </div>
   <div class="group">
-    <input id="email" type="email" required placeholder="Votre email..."/>
+    <input id="email" name="email" type="email" required placeholder="Votre email..."/>
     <label for="email">Email</label>
   </div>
   <div class="group">
-    <input id="password" type="password" required placeholder="Votre mot de passe..."/>
+    <input id="password" name="password" type="password" required placeholder="Votre mot de passe..."/>
     <label for="password">Mot de passe</label>
   </div>
 </template>
