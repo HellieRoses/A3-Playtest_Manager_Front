fbbd533774eb7af4c79b3354bb64dd9e27b9c4fe
Touze Romain <romain.touze@etu.umontpellier.fr>
2024-12-21 18:13:48 +0000

    fix espace PlaytestForm

--- src/views/Playtest/CreatePlaytest.vue
+++ src/views/Playtest/CreatePlaytest.vue
@@ -11,7 +11,7 @@ const videoGames = ["Assassin's  Creed", "Hollow Knight", "Celeste", "Hades 2", 
     </div>
 
     <form @submit.prevent=""> <!-- fonction créer playtest-->
-      <Playte stForm :video-games="videoGames" />
+      <PlaytestForm :video-games="videoGames" />
       <div class="bottom-button">
         <button type="submit" class="button">
           <p>Créer</p>
